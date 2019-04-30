fef25570038fac31e6fd7f0871d2954004374601
samigor <47845766+samigor@users.noreply.github.com>
2019-04-30 16:56:25 +0300

    1mail

--- app/Http/Controllers/mailController.php
+++ app/Http/Controllers/mailController.php
@@ -4,13 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Mail;
+use App\mail\newMail;
 class mailController extends Controller
 {
     public function send ()
  {
-   Mail::send(['text' => 'mail'], ['name', 'Siminchenko'], function ($message){
+   Mail::send(new newMail());
+   /*Mail::send(['text' => 'mail'], ['name', 'Siminchenko'], function ($message){
 
      $message ->to('igor@kntu.net.ua', 'to KNTU mail')->subject('Test email');
      $message ->from('siminchenko@gmail.com','Igor mail');
-   });
- }}
+   });*/
+ }
+ public function email ()
+ {
+return view('email');
+ }
+}
